22621901c4411e2b8249e5a9362fd1093c231b14
Marek <marekroeder@gmx.de>
2023-11-17 12:39:45 +0100

    fix: normal computation now uses the bitangent, t is almost correct

--- src/core/instance.cpp
+++ src/core/instance.cpp
@@ -7,26 +7,35 @@ namespace lightwave {
 
 void Instance::transformFrame(SurfaceEvent &surf) const {
 
+    Vector t = m_transform->apply(surf.frame.tangent).normalized();
+    Vector b = m_transform->apply(surf.frame.bitangent).normalized();
+
     surf.position = m_transform->apply(surf.position);
+    /*
 
     Vector tangent = m_transform->apply(surf.frame.tangent);
     //Vector tangent = surf.frame.tangent;
     Vector bitangent = surf.frame.bitangent;
     Vector normal = surf.frame.normal;
 
-    Vector new_bitangent = tangent.cross(normal);
+    Vector new_bitangent = tangent.cross(normal); */
 
     if (m_flipNormal) {
-        surf.frame.bitangent = - new_bitangent.normalized();
+        //surf.frame.bitangent = - new_bitangent.normalized();
+        b = -b;
     }
-
+    Vector n = t.cross(b).normalized();
+    buildOrthonormalBasis(n,t,b);
+    surf.frame.normal = n;
+    surf.frame.tangent = t;
+    surf.frame.bitangent = b;
 
     //Vector new_normal = new_bitangent.cross(tangent);
-    Vector new_normal = m_transform->inverse(normal);
+   // Vector new_normal = m_transform->inverse(normal);
     //surf.frame.bitangent = new_bitangent.normalized();
     
     //surf.frame.normal = m_transform->apply(new_normal).normalized();
-    surf.frame.normal = new_normal.normalized();
+    //surf.frame.normal = new_normal.normalized();
 
 
 
@@ -50,14 +59,15 @@ bool Instance::intersect(const Ray &worldRay, Intersection &its, Sampler &rng) c
     }
 
     const float previousT = its.t;
-    Vector previous_hitpoint_vector = its.position - Point(0);
-    Ray localRay;
-    float previous_hitpoint_x = previous_hitpoint_vector.x();
-    Vector new_hitpoint_vector = m_transform->apply(previous_hitpoint_vector);
-    float new_hitpoint_x = new_hitpoint_vector.x();
-    localRay = m_transform->inverse(worldRay);
-    localRay = localRay.normalized();
-    float t_factor = previous_hitpoint_x / new_hitpoint_x;
+    //Vector previous_hitpoint_vector = its.position - Point(0);
+    //Ray localRay;
+    //float previous_hitpoint_x = previous_hitpoint_vector.x();
+    //Vector new_hitpoint_vector = m_transform->apply(previous_hitpoint_vector);
+    //float new_hitpoint_x = new_hitpoint_vector.x();
+    Ray localRay = m_transform->inverse(worldRay);
+    localRay.direction = localRay.direction.normalized();
+    //localRay = localRay.normalized();
+    //float t_factor = previous_hitpoint_x / new_hitpoint_x;
     //its.t = previousT * t_factor;
 
 
@@ -70,12 +80,11 @@ bool Instance::intersect(const Ray &worldRay, Intersection &its, Sampler &rng) c
 
     const bool wasIntersected = m_shape->intersect(localRay, its, rng);
     if (wasIntersected) {
-        Point transformed_hitpoint = m_transform->inverse(worldRay(its.t));
-
-
-        Vector p_o = transformed_hitpoint - localRay.origin;
-        float new_t = p_o.x() / localRay.direction.x();
-        its.t = new_t;
+        //Point transformed_hitpoint = m_transform->inverse(worldRay(its.t));
+        Point transformed_hitpoint = m_transform->apply(localRay(its.t));
+        its.t = (transformed_hitpoint - worldRay.origin).x() / worldRay.direction.x();
+        //float new_t = p_o.y() / worldRay.direction.y();
+        //its.t = new_t;
         its.instance = this;
         transformFrame(its);
         return true;
