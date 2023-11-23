580775c047469ae03bc7176933f0003c81cc7e84
Marek <marekroeder@gmx.de>
2023-11-23 17:17:19 +0100

    feat: border_mode_clamp function and idea for a border_mode_repeat function

--- src/textures/image.cpp
+++ src/textures/image.cpp
@@ -53,7 +53,44 @@ public:
                            "]",
                            indent(m_image), m_exposure);
     }
+private:
+    // Maps from [-infinity, +infinity] to [0,1]
+    Point2 border_mode_clamp(const Point2 &uv) {
+        float u = saturate(uv.x());
+        float v = saturate(uv.y());
+        return Point2(u,v);
+    }
+
+    Point2 border_mode_repeat(const Point2 &uv) {
+        /* Idea
+        We need a function that does the following
+        -1 -> 0
+        -0.5 -> 0.5 
+        -0.999...9 -> 1
+        0 -> 0
+        0.5 -> 0.5
+        1 -> 1
+        1.000...1 -> 0
+        1.5 -> 0.5
+        2 -> 1
+
+        for negativ numbers this is just mod 1
+
+        for positiv numbers:
+        mod 1 works almost everywhere
+        but not for 2 -> 1 and -1 ->0
+        hence use if(i > 0 && i mod 1 == 0)
+        BUT this will result in problems with floating point numbers
+        
+        */
+        float u; 
+        float v;
+        return Point2(u,v);
+    }
+
 };
+
+
 
 } // namespace lightwave
 
