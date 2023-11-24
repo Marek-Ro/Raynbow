77a286b2cc1187143ea2a16861e4e97879ae3f42
Marek <marekroeder@gmx.de>
2023-11-24 14:36:57 +0100

    fix: fix fix

--- src/textures/image.cpp
+++ src/textures/image.cpp
@@ -61,7 +61,7 @@ public:
             pixel = filter_mode_nearest(point);
         }
         else {
-            pixel = filter_mode_bilinear(point);
+            return filter_mode_bilinear(point);
         }
 
         // making sure pixel is in image
@@ -94,39 +94,10 @@ private:
         } else if (x > 1) {
             return help_mod_1(x - 1);
         }
+        return 0;
     }
 
     Point2 border_mode_repeat(const Point2 &uv) const {
-        // TODO
-
-        /* Idea
-        We need a function that does the following
-        -1 -> 0
-        -0.5 -> 0.5 
-        -0.999...9 -> 1
-        0 -> 0
-        0.5 -> 0.5
-        1 -> 1
-        1.000...1 -> 0.00000001
-        1.5 -> 0.5
-
-        1.99999999 -> 0.99999999 = 1
-        2 -> 1
-        2 - 1
-        2.00000000000001 -> 0.0000000001 = 0
-        10,9 - 10
-        for negativ numbers this is just mod 1
-
-        for positiv numbers:
-        mod 1 works almost everywhere
-        but not for 2 -> 1
-        hence use if(i > 0 && i mod 1 == 0)
-        BUT this will result in problems with floating point numbers
-        
-        */
-//        float u = fmod(uv.x(), 1.0); 
-//        float v = fmod(uv.y(), 1.0);
-
         float u = help_mod_1(uv.x()); 
         float v = help_mod_1(uv.y());
 
@@ -141,41 +112,40 @@ private:
         return pixel;
     }
 
-    Point2i filter_mode_bilinear(const Point2 point) const {
-        // from: https://en.wikipedia.org/wiki/Bilinear_interpolation
-        //
-        // we got a point that lies between four other pixels
-        // round x and y down and up 
-        // Example:
-        // (1.2, 2,9) -> Q11(1,2), Q12(1,3), Q21(2,2), Q22(2,3)
-        //            -> Q11(x1,y1), Q12(x1,y2), Q21(x2,y1), Q22(x2,y2)
+    Color filter_mode_bilinear(const Point2 point) const {
+        Point2i Q11, Q12, Q21, Q22;
+        Q11.x() = floor(point.x());
+        Q11.y() = floor(point.y());
+        Q12.x() = floor(point.x());
+        Q12.y() = floor(point.y()+1.0f);
+        Q21.x() = floor(point.x()+1.0f);
+        Q21.y() = floor(point.y());
+        Q22.x() = floor(point.x()+1.0f);
+        Q22.y() = floor(point.y()+1.0f);
         
-        // TODO evt. direkt zu point2i machen weil rounding error später
-        Point2 Q11, Q12, Q21, Q22  = point;
-        Q11.x() = floor(Q11.x());
-        Q11.y() = floor(Q11.y());
-
-        Q12.x() = floor(Q12.x());
-        Q12.y() = floor(Q12.y()+1.0f);
-
-        Q21.x() = floor(Q21.x()+1.0f);
-        Q21.y() = floor(Q21.y());
-
-        Q22.x() = floor(Q22.x()+1.0f);
-        Q22.y() = floor(Q22.y()+1.0f);
-
-
-        // linear interpolation in the x-Direction
-        Point2 FX1 = ((Q21.x() - point.x())/(Q21.x() - Q11.x()))* (Q11-Point2(0)) + ((point.x() - Q11.x())/(Q21.x() - Q11.x()))* (Q21-Point2(0));
-        Point2 FX2 = ((Q21.x() - point.x())/(Q21.x() - Q11.x()))* (Q12-Point2(0)) + ((point.x() - Q11.x())/(Q21.x() - Q11.x()))* (Q22-Point2(0));
+        //x1
+        float f = Q11.x() - point.x();
+        Color a = m_image->operator()(Q11);
+        Color b = m_image->operator()(Q21);
+        Color c1 = lin_interpolate(a,b,f);
         
-        // linear interpolation in the y-Direction
-        Point2 FXY = ((Q22.y() - point.y())/(Q22.y() - Q11.y()))* (FX1-Point2(0)) + ((point.y() - Q11.y())/(Q22.y() - Q11.y()))* (FX2-Point2(0));
+        //x2
+        f = Q12.x() - point.x();
+        a = m_image->operator()(Q12);
+        b = m_image->operator()(Q22);
+        Color c2 = lin_interpolate(a,b,f);
+        
+        // y1
+        f = Q11.y() - point.y();
+        Color c3 = lin_interpolate(c1,c2,f);
 
 
-        Point2i pixel = Point2i(FXY.x(), FXY.y());
-        //return filter_mode_nearest(point);
-        return pixel;
+        Color color;
+        return c3;
+    }
+
+    Color lin_interpolate(Color a, Color b, float f) const {
+        return (a * (1.0 - f)) + (b * f);
     }
 
 };
