004a2ac5f66a69a9e9aea5857955c043f26aa297
maro00008 <maro00008@stud.uni-saarland.de>
2023-12-02 15:41:03 +0100

    code cleanup and comments in envmap

--- src/lights/envmap.cpp
+++ src/lights/envmap.cpp
@@ -1,68 +1,70 @@
 #include <lightwave.hpp>
 
-namespace lightwave {
+namespace lightwave
+{
 
-class EnvironmentMap final : public BackgroundLight {
-    /// @brief The texture to use as background
-    ref<Texture> m_texture;
+    class EnvironmentMap final : public BackgroundLight
+    {
+        /// @brief The texture to use as background
+        ref<Texture> m_texture;
 
-    /// @brief An optional transform from local-to-world space
-    ref<Transform> m_transform;
+        /// @brief An optional transform from local-to-world space
+        ref<Transform> m_transform;
 
-public:
-    EnvironmentMap(const Properties &properties) {
-        m_texture   = properties.getChild<Texture>();
-        m_transform = properties.getOptionalChild<Transform>();
-    }
-
-    BackgroundLightEval evaluate(const Vector &direction) const override {
-        Vector2 warped = Vector2(0, 0);
-        // hints:
-        // * if (m_transform) { transform direction vector from world to local
-        // coordinates }
-        // * find the corresponding pixel coordinate for the given local
-        // direction
-
-        float theta, phi;
-        Vector direction2 = direction.normalized();
-        if (m_transform) {
-            direction2 = m_transform->inverse(direction).normalized();
-            
+    public:
+        EnvironmentMap(const Properties &properties)
+        {
+            m_texture = properties.getChild<Texture>();
+            m_transform = properties.getOptionalChild<Transform>();
         }
 
-        theta = safe_acos(direction2.y());
-        phi = atan2(-direction2.z(),direction2.x());
-        warped = Vector2((phi+Pi)*Inv2Pi, theta * InvPi);
+        BackgroundLightEval evaluate(const Vector &direction) const override
+        {
+            Vector direction2 = direction.normalized();
 
-        return {
-            .value = m_texture->evaluate(warped),
-        };
-    }
+            // transform from world to local coordinates
+            if (m_transform)
+            {
+                direction2 = m_transform->inverse(direction).normalized();
+            }
 
-    DirectLightSample sampleDirect(const Point &origin,
-                                   Sampler &rng) const override {
-        Vector direction = squareToUniformSphere(rng.next2D());
-        auto E           = evaluate(direction);
+            // convert from cartesian to spherical coordinates
+            float theta = safe_acos(direction2.y());
+            float phi = atan2(-direction2.z(), direction2.x());
+            // map the spherical coordinates to [0;1]
+            Vector2 warped = Vector2((phi + Pi) * Inv2Pi, theta * InvPi);
 
-        // implement better importance sampling here, if you ever need it
-        // (useful for environment maps with bright tiny light sources, like the
-        // sun for example)
+            return {
+                .value = m_texture->evaluate(warped),
+            };
+        }
 
-        return {
-            .wi     = direction,
-            .weight = E.value / Inv4Pi,
-            .distance = Infinity,
-        };
-    }
+        DirectLightSample sampleDirect(const Point &origin,
+                                       Sampler &rng) const override
+        {
+            Vector direction = squareToUniformSphere(rng.next2D());
+            auto E = evaluate(direction);
 
-    std::string toString() const override {
-        return tfm::format("EnvironmentMap[\n"
-                           "  texture = %s,\n"
-                           "  transform = %s\n"
-                           "]",
-                           indent(m_texture), indent(m_transform));
-    }
-};
+            // implement better importance sampling here, if you ever need it
+            // (useful for environment maps with bright tiny light sources, like the
+            // sun for example)
+
+            return {
+                .wi = direction,
+                .weight = E.value / Inv4Pi,
+                .distance = Infinity,
+            };
+        }
+
+        std::string toString() const override
+        {
+            return tfm::format("EnvironmentMap[\n"
+                               "  texture = %s,\n"
+                               "  transform = %s\n"
+                               "]",
+                               indent(m_texture), indent(m_transform));
+        }
+    };
 
 } // namespace lightwave
 
