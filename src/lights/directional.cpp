01680b4c06b5340ff6707cc4c99da5f9941aa13d
Tobias Wiedemann <towi00001@stud.uni-saarland.de>
2023-12-04 14:22:45 +0100

    Directional Lighting

---
+++ src/lights/directional.cpp
@@ -0,0 +1,36 @@
+#include <lightwave.hpp>
+
+namespace lightwave {
+
+class DirectionalLight final : public Light {
+public:
+    Color intensity;
+    Vector direction;
+    
+    DirectionalLight(const Properties &properties) {
+        direction = properties.get<Vector>("direction");
+        intensity = properties.get<Color>("intensity");
+    }
+
+    DirectLightSample sampleDirect(const Point &origin,
+                                   Sampler &rng) const override {
+        
+        DirectLightSample d = DirectLightSample {
+            .wi = direction,
+            .weight = intensity,
+            .distance = Infinity,
+        };
+        return d;
+    }
+
+    bool canBeIntersected() const override { return false; }
+
+    std::string toString() const override {
+        return tfm::format("DirectionalLight[\n"
+                           "]");
+    }
+};
+
+} // namespace lightwave
+
+REGISTER_LIGHT(DirectionalLight, "directional")
