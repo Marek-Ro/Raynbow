f035a3d152174564e1aead86f0bdcc770c6e38c0
Tobias Wiedemann <towi00001@stud.uni-saarland.de>
2023-12-04 14:17:41 +0100

    point light, black image

--- src/lights/point.cpp
+++ src/lights/point.cpp
@@ -4,12 +4,23 @@ namespace lightwave {
 
 class PointLight final : public Light {
 public:
+    Color weight;
+    Point position;
+    
     PointLight(const Properties &properties) {
+        position = properties.get<Point>("position");        
+        weight = properties.get<Color>("power");
     }
 
     DirectLightSample sampleDirect(const Point &origin,
                                    Sampler &rng) const override {
-        NOT_IMPLEMENTED
+        
+        DirectLightSample d = DirectLightSample {
+            .wi = origin - position,
+            .weight = weight * Inv4Pi,
+            .distance = (origin - position).length(),
+        };
+        return d;
     }
 
     bool canBeIntersected() const override { return false; }
