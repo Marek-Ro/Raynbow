71e12930a0c4f381cf73f38ea78532ebedee2339
Tobias Wiedemann <towi00001@stud.uni-saarland.de>
2023-12-14 23:55:53 +0100

    Working on thinlens with pbrt, no fov_axis regarded yet, just seeing white so far

--- src/cameras/thinlense.cpp
+++ src/cameras/thinlense.cpp
@@ -20,7 +20,11 @@ public:
     int width;
     int height;
     std::string fov_axis;
-    float focal_length;
+
+// Thinlens
+//    float depth_of_field;
+    float focalDistance;
+    float lensRadius;
 
     Thinlens(const Properties &properties)
     : Camera(properties) {
@@ -34,6 +38,8 @@ public:
         // aspect ratio depends on fov_axis
         aspect_ratio = fov_axis == "y" ? (float)width / (float)height : (float)height / (float)width;
 
+        lensRadius = 0.1;
+        focalDistance = 0.01;
         // hints:
         // * precompute any expensive operations here (most importantly trigonometric functions)
         // * use m_resolution to find the aspect ratio of the image
@@ -58,18 +64,57 @@ public:
         xy = xy.normalized();
 
         // ray with origin (0,0,0) and direction vector xy
-        Ray ray = Ray(xy, xy - Vector(0, 0, -30));
+        Ray ray = Ray(Vector(0.f, 0.f, 0.f), xy);
+
+
+
+        // pbrt thinlens here
+
+        // x and y are in [0, 1) already
+
+        // Point on the lens
+        Vector2 pLens = lensRadius * sampleUniformDiskConcentric(Vector2(normalized.x(), normalized.y()));
+
+        // compute point on the plane of focus
+        float ft = focalDistance / ray.direction.z();
+        Point pFocus = ray(ft);
+
+        // update ray for effect of lens
+        ray.origin = Point(pLens.x(), pLens.y(), 0);
+        ray.direction = (pFocus - ray.origin).normalized();
+
+
+
+
+
+        // finally transform to world space
         ray = m_transform->apply(ray).normalized();
-
-//        ray.direction += Vector(0, 0.1, 0);
-//        ray.direction = ray.direction.normalized();        
-
-//        ray.direction = m_transform->apply(Vector(0, 0, 1)).normalized();
 
         Color weight = Color(1.0f);
         return CameraSample{
             ray, weight
         };
+    }
+
+    Vector2 sampleUniformDiskConcentric(Vector2 u) const {
+        
+        // remap u from [0, 1] to [-1, 1]
+        Vector2 uOffset = (2 * u) - Vector2(1, 1);
+        if (uOffset.x() == 0 && uOffset.y() == 0) {
+            return uOffset;
+        }
+
+        // apply concentric mapping to the point
+        float theta, r;
+        if (std::abs(uOffset.x()) > std::abs(uOffset.y())) {
+            r = uOffset.x();
+            theta = (Pi / 4) * (uOffset.y() / uOffset.x());
+        } else {
+            r = uOffset.y();
+            theta = (Pi / 2) - (Pi / 4) * (uOffset.x() / uOffset.y());
+        }
+
+        return r * Vector2(std::cos(theta), std::sin(theta));
     }
 
     std::string toString() const override {
