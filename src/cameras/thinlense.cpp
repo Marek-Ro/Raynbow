1d404b02dedb9dc6bf9328e12b75a4d4f30f79d5
Tobias Wiedemann <towi00001@stud.uni-saarland.de>
2023-12-14 20:32:13 +0100

    playing around and thinlens

---
+++ src/cameras/thinlense.cpp
@@ -0,0 +1,91 @@
+#include <lightwave.hpp>
+#include <numbers>
+
+namespace lightwave {
+
+/**
+ * @brief A perspective camera with a given field of view angle and transform.
+ * 
+ * In local coordinates (before applying m_transform), the camera looks in positive z direction [0,0,1].
+ * Pixels on the left side of the image ( @code normalized.x < 0 @endcode ) are directed in negative x
+ * direction ( @code ray.direction.x < 0 ), and pixels at the bottom of the image ( @code normalized.y < 0 @endcode )
+ * are directed in negative y direction ( @code ray.direction.y < 0 ).
+ */
+class Thinlens : public Camera {
+public:
+
+    float fov;
+    float aspect_ratio;
+    float BC; // stores: tan(fov / 2)
+    int width;
+    int height;
+    std::string fov_axis;
+    float focal_length;
+
+    Thinlens(const Properties &properties)
+    : Camera(properties) {
+        fov = properties.get<float>("fov");
+        width = properties.get<int>("width");
+        height = properties.get<int>("height");
+        // From degree to radian 
+        float radian = fov / (float)360 * 2 * (float)std::numbers::pi;
+        BC = tan(radian / 2);
+        fov_axis = properties.get<std::string>("fovAxis");
+        // aspect ratio depends on fov_axis
+        aspect_ratio = fov_axis == "y" ? (float)width / (float)height : (float)height / (float)width;
+
+        // hints:
+        // * precompute any expensive operations here (most importantly trigonometric functions)
+        // * use m_resolution to find the aspect ratio of the image
+    }
+
+    CameraSample sample(const Point2 &normalized, Sampler &rng) const override {
+
+        // Transforming normalized image coordinates to normalized camera coordinates
+        float x = normalized.x() * BC;
+        float y = normalized.y() * BC;
+        
+        // apply aspect_ratio depending on the fov_axis
+        if (fov_axis == "y") {
+            x = x * aspect_ratio;
+        } else if (fov_axis == "x"){
+            y = y * aspect_ratio;
+        }
+
+        // create direction vector, remap on imaginary plane z=1 (camera direction is [0,0,1])
+        // normalize important for the ray
+        Vector xy = Vector(x,y, 1.f);
+        xy = xy.normalized();
+
+        // ray with origin (0,0,0) and direction vector xy
+        Ray ray = Ray(xy, xy - Vector(0, 0, -30));
+        ray = m_transform->apply(ray).normalized();
+
+//        ray.direction += Vector(0, 0.1, 0);
+//        ray.direction = ray.direction.normalized();        
+
+//        ray.direction = m_transform->apply(Vector(0, 0, 1)).normalized();
+
+        Color weight = Color(1.0f);
+        return CameraSample{
+            ray, weight
+        };
+    }
+
+    std::string toString() const override {
+        return tfm::format(
+            "Thinlens[\n"
+            "  width = %d,\n"
+            "  height = %d,\n"
+            "  transform = %s,\n"
+            "]",
+            m_resolution.x(),
+            m_resolution.y(),
+            indent(m_transform)
+        );
+    }
+};
+
+}
+
+REGISTER_CAMERA(Thinlens, "thinlens")
