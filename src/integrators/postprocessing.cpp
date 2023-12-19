46e805ed4b2d886e93a9e36bcf2d2127cd5d0470
maro00008 <maro00008@stud.uni-saarland.de>
2023-12-19 12:41:35 +0100

    ImageDenoiseExample does not work

--- src/integrators/postprocessing.cpp
+++ src/integrators/postprocessing.cpp
@@ -1,17 +1,15 @@
 // While the denoiser might only require your rendered image as input, you will also want to feed images of normals, albedo
 
 #include <lightwave.hpp>
-
+#include <OpenImageDenoise/oidn.hpp>
 namespace lightwave {
 
 
 class denoise : public Postprocess {
-    // image
-    // normals
-    // albedo
- 
-public:
-
+    oidn::DeviceRef device = oidn::newDevice();
+    //device.commit();
+    // does not work?
+    // https://www.openimagedenoise.org/documentation.html
 
 };
 
