27e2aa8b00edfc5317e7cdb42dfb3214bb5512aa
Tobias Wiedemann <towi00001@stud.uni-saarland.de>
2023-12-24 00:14:25 +0100

    small progress, partially following teams posts

--- src/Postprocessing/denoise.cpp
+++ src/Postprocessing/denoise.cpp
@@ -9,6 +9,7 @@ class denoise : public Postprocess {
 public:
     denoise(const Properties &properties)
         : Postprocess(properties) {
+            m_output->initialize(m_input->resolution());
     }
     void execute() override {
         float width, height;
@@ -16,20 +17,23 @@ public:
         height = m_input->resolution().y();
 
         // Create an Open Image Denoise device
-        oidn::DeviceRef device = oidn::newDevice();
+        oidn::DeviceRef device = oidn::newDevice(oidn::DeviceType::CPU);
         device.commit();
 
         // Create buffers for input/output images accessible by both host (CPU) and device (CPU/GPU)
         oidn::BufferRef colorBuf  = device.newBuffer(width * height * 3 * sizeof(float));
         //oidn::BufferRef albedoBuf = ...
+        oidn::BufferRef albedoBuf = device.newBuffer(width * height * 3 * sizeof(float));
+        oidn::BufferRef normalBuf = device.newBuffer(width * height * 3 * sizeof(float));
+
 
         // Create a filter for denoising a beauty (color) image using optional auxiliary images too
         // This can be an expensive operation, so try no to create a new filter for every image!
         oidn::FilterRef filter = device.newFilter("RT"); // generic ray tracing filter
-        filter.setImage("color",  colorBuf,  oidn::Format::Float3, width, height); // beauty
-        //filter.setImage("albedo", albedoBuf, oidn::Format::Float3, width, height); // auxiliary
-        //filter.setImage("normal", normalBuf, oidn::Format::Float3, width, height); // auxiliary
-        filter.setImage("output", colorBuf,  oidn::Format::Float3, width, height); // denoised beauty
+        filter.setImage("color",  colorBuf,  oidn::Format::Float, width, height); // beauty
+        filter.setImage("albedo", albedoBuf, oidn::Format::Float, width, height); // auxiliary
+        filter.setImage("normal", normalBuf, oidn::Format::Float, width, height); // auxiliary
+        filter.setImage("output", colorBuf,  oidn::Format::Float, width, height); // denoised beauty
         filter.set("hdr", true); // beauty image is HDR
         filter.commit();
 
